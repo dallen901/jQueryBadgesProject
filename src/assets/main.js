910a3ee5b2bf2aa1acb1bff86cb319ccac31a74b
David Allen <dallen1@unum.com>
2018-02-28 12:09:09 -0500

    initial commit

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,5 +1,17 @@
 $(function() {
-
-  // your code will go here
-
+  $.ajax({
+    url: 'https://www.codeschool.com/users/3553017.json',
+    dataType: 'jsonp',
+    success: function(response) {
+      $.each(response.courses.completed, function(index, course) {
+        var div = $('<div></div>').addClass('course');
+        var header = $('<h3></h3>').text(course.title);
+        var img = $('<img src= \"' + course.badge + '\"/>');
+        var a = $('<a href=\"' + course.url + '\" target=\"_blank\" class=\"btn btn-primary\"></a>').text("See Course");
+        console.log(course.url)
+        div.append(header, img, a);
+        $('#badges').append(div);
+      });
+    }
+  });
 });
